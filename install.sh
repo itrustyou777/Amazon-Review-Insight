adc94d81a97c8347f5b5ef2538005e2e0196e26c
Miji <itrustyou777@gmail.com>
2018-05-10 00:23:16 -0400

    Added support scripts

---
+++ install.sh
@@ -0,0 +1,3 @@
+sudo apt-get install postgresql 
+sudo pip install flask psycopg2 boto3
+wget https://jdbc.postgresql.org/download/postgresql-42.2.2.ja
---
+++ vars.sh
@@ -0,0 +1,13 @@
+# Sets the required configuration for connecting to the database 
+# source this file
+
+export RDS_HOST=insight-test.c0lqcrqaigco.us-east-1.rds.amazonaws.com
+export RDS_DB=amazon_review_insight
+export RDS_PORT=5432
+export RDS_USER=mijik
+
+# RDS_PASSWORD is set in ~/.profile
+# AWS_ACCESS_KEY_ID is set in ~/.profile
+# AWS_SECRET_ACCESS_KEY is set in ~/.profile
+export AWS_DEFAULT_REGION=us-east-1
+
