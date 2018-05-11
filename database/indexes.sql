6e8e0a2eed1a0f4c463423a7ae27e378a16bff87
Miji <itrustyou777@gmail.com>
2018-05-10 22:17:11 -0400

    Added indexes

--- database/indexes.sql
+++ database/indexes.sql
@@ -1,6 +1,14 @@
 -- Indexes 
 
+ CREATE INDEX reviews_sentiment_time_index ON reviews (asin, sentiment, (cast(extract(year from "reviewTime") as text) || '-' || cast(extract(month from
+"reviewTime") as text)));
+
+CLUSTER reviews USING reviews_sentiment_time_index;
+
+
 CREATE INDEX products_title_index ON products USING GIN (to_tsvector('english', title));
 CREATE INDEX reviews_summary_and_text_index ON reviews USING GIN (to_tsvector('english', summary || ' ' || "reviewText"));
-CREATE INDEX product_overall_desc_index ON reviews (overall DESC);
-CREATE INDEX product_overall_asc_index ON reviews (overall ASC);
+
+CREATE INDEX reviews_overall_desc_index ON reviews (overall DESC);
+CREATE INDEX reviews_overall_asc_index ON reviews (overall ASC);
+
