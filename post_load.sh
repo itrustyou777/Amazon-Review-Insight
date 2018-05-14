940e8760405c94b0355f6172acff18027266649d
Miji <itrustyou777@gmail.com>
2018-05-13 22:05:02 -0400

    Added a comment, and deleted a wrong file

--- post_load.sh
+++ post_load.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This file is following after batach processing to built indexes and vacumming
+
 ./database/run_sql.sh database/constrains.sql
 ./database/run_sql.sh database/indexes.sql
 ./database/run_sql.sh database/vacumme_and_analyze.sql
